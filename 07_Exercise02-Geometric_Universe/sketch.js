61a75fef99a8e6b74f14af940572e835bdd6ad26
blangham <benlangham@pc-72-46.customer.beckley.tshc.lan>
2017-12-10 18:47:29 +0000

    update

---
+++ 07_Exercise02-Geometric_Universe/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(1000, 500);
+  strokeWeight(8);
+  noFill();
+  noLoop();
+}
+
+function draw() {
+  background(50);
+  for (var i = 0; i < 300; i++) {
+    push();
+    translate(random(width), random(height));
+    rotate(random(2*PI));
+    scale(random(1.5), random(1.5));
+    stroke(random(255), random(255), random(255));
+    ellipse(0, 0, 40, 40);
+    pop();
+  }
+}
